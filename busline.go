46bf9206bab5f96acef5e11642ce2c1e63a541eb
Rodrigo de Castro <castrodrigo@yahoo.com.br>
2016-10-08 15:00:27 -0300

    adding query method by Id, mock data and server running

--- busline.go
+++ busline.go
@@ -19,3 +19,38 @@ type Route struct {
     From      string `json:"from,omitempty"`
     To        string `json:"to,omitempty"`
 }
+
+var busline []BusLine
+
+// GET
+
+func GetBusLineById(writer http.ResponseWriter, request *http.Request) {
+    params := mux.Vars(request)
+    for _, item := range busline {
+        if item.ID == params["id"] {
+            json.NewEncoder(writer).Encode(item)
+            return
+        }
+    }
+}
+
+
+
+// MAIN
+
+func main() {
+    busline = append(busline, BusLine {
+            ID: "1",
+            Name: "Manoel Torres",
+            Number: "107",
+            Route: &Route {
+                    From: "Bingen Bus Terminal",
+                    To: "Historic Center Bus Terminal" }})
+
+    router := mux.NewRouter()
+    router.HandleFunc("/busLine/{id}", GetBusLineById).Methods("GET")
+
+    // RUNNING PORT
+
+    log.Fatal(http.ListenAndServe(":9009", router))
+}
